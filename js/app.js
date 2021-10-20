3c91fc436e2a63118413fe632d87da57c7ca2b1e
gentlevulture <novemberboughs@gmail.com>
2021-10-20 10:36:21 -0700

    reAssessment

--- js/app.js
+++ js/app.js
@@ -1,22 +1,18 @@
 const input1 = document.getElementById("math-input1")
-const mathResult = parseInt(document.getElementById("math-result").value)
+const mathResult =document.getElementById("math-result")
 const plusButton = document.getElementById("plus")
 const minusButton = document.getElementById("minus")
 
 plusButton.addEventListener("click", add)
 minusButton.addEventListener("click", subtract)
 
-.operator.addEventListener("mouseover", function( event ) {
-  event.target.style.color = "gray";
-}, false);
-
 function add(evt) {
-  const result = parseInt(input1.value) + mathResult.value
+  const result = parseInt(input1.value) + parseInt(mathResult.innerHTML)
   mathResult.textContent = result
 }
 
 function subtract(evt) {
-  const result = parseInt(input1.value) - mathResult.value
-  mathResult.textContent = result
+  const result = parseInt(mathResult.innerHTML) - parseInt(input1.value)
+  mathResult.textContent = result 
 }
 
